b335b7d12ccb074a49260be3e12d1f3c2c72ac45
chacha <chacha@sparcs.org>
2024-10-30 00:38:17 +0900

    fix: delete comments

--- packages/web/stories/Button.stories.tsx
+++ packages/web/stories/Button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-// import { fn } from "@storybook/addon-actions";
 import { fn } from "@storybook/test";
 
 import Button from "@sparcs-students/web/common/components/Buttons/Button";
