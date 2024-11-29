428ee1af581343dd1a9890b540520cbf1ccfab8e
Gerbera3090 <hw3090@gmail.com>
2024-11-30 04:50:49 +0900

    feat: api interface for apiprp004

--- packages/interface/src/api/proposal/endpoint/apiPrp004.ts
+++ packages/interface/src/api/proposal/endpoint/apiPrp004.ts
@@ -1,17 +1,18 @@
 import { HttpStatusCode } from "axios";
 import { z } from "zod";
 
-import { zOrgName } from "@sparcs-students/interface/common/stringLength";
 import { zId } from "@sparcs-students/interface/common/type/ids";
 
 /**
  * @version v0.1
- * @description 기구장단 권한으로 새로운 팀을 생성합니다.
+ * @description 기구장단 권한으로 새로운 project proposal 을 만들어 냅니다.
+ * 첫번째 revision도 같이 생성합니다.
+ *
  */
 
 const url = () => `/president/organizations/teams/team`;
 const method = "POST";
-export const ApiOrg007RequestUrl = "/president/organizations/teams/team";
+export const ApiPrp004RequestUrl = "/president/organizations/teams/team";
 
 const requestParam = z.object({});
 
@@ -20,8 +21,7 @@ const requestQuery = z.object({});
 const requestBody = z.object({
   organizationId: zId,
   semesterId: zId,
-  name: zOrgName,
-  detail: z.coerce.string(),
+  name: z.coerce.string().max(30),
 });
 
 const responseBodyMap = {
@@ -32,7 +32,7 @@ const responseBodyMap = {
 
 const responseErrorMap = {};
 
-const apiOrg007 = {
+const apiPrp004 = {
   url,
   method,
   requestParam,
@@ -42,18 +42,18 @@ const apiOrg007 = {
   responseErrorMap,
 };
 
-type ApiOrg007RequestParam = z.infer<typeof apiOrg007.requestParam>;
-type ApiOrg007RequestQuery = z.infer<typeof apiOrg007.requestQuery>;
-type ApiOrg007RequestBody = z.infer<typeof apiOrg007.requestBody>;
-type ApiOrg007ResponseCreated = z.infer<
-  (typeof apiOrg007.responseBodyMap)[201]
+type ApiPrp004RequestParam = z.infer<typeof apiPrp004.requestParam>;
+type ApiPrp004RequestQuery = z.infer<typeof apiPrp004.requestQuery>;
+type ApiPrp004RequestBody = z.infer<typeof apiPrp004.requestBody>;
+type ApiPrp004ResponseCreated = z.infer<
+  (typeof apiPrp004.responseBodyMap)[201]
 >;
 
-export default apiOrg007;
+export default apiPrp004;
 
 export type {
-  ApiOrg007RequestParam,
-  ApiOrg007RequestQuery,
-  ApiOrg007RequestBody,
-  ApiOrg007ResponseCreated,
+  ApiPrp004RequestParam,
+  ApiPrp004RequestQuery,
+  ApiPrp004RequestBody,
+  ApiPrp004ResponseCreated,
 };
