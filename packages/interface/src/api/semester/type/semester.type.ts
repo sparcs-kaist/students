3c2b808ce96f54051825718858012539c55f5ccd
Gerbera3090 <hw3090@gmail.com>
2025-01-21 17:52:14 +0900

    feat: time type and semester type

---
+++ packages/interface/src/api/semester/type/semester.type.ts
@@ -0,0 +1,25 @@
+import { HalfYearE, SemesterE } from "@sparcs-students/interface/common/enum";
+import { zDuration } from "@sparcs-students/interface/common/type/time.type";
+import { z } from "zod";
+
+export const zSemester = z.object({
+  id: z.string(),
+  name: z.string(),
+  year: z.number(),
+  semesterEnum: z.nativeEnum(SemesterE),
+  duration: zDuration,
+});
+
+export type ISemester = z.infer<typeof zSemester>;
+
+export const zHalfYear = z.object({
+  id: z.string(),
+  name: z.string(), // ex) 2024년 상반기
+  year: z.number(),
+  halfYearEnum: z.nativeEnum(HalfYearE),
+  duration: zDuration,
+  regularSemester: zSemester,
+  seasonalSemester: zSemester,
+});
+
+export type IHalfYear = z.infer<typeof zHalfYear>;
--- packages/interface/src/common/enum/index.ts
+++ packages/interface/src/common/enum/index.ts
@@ -1,3 +1,4 @@
 export * from "./budget.enum";
 export * from "./meeting.enum";
 export * from "./organization.enum";
+export * from "./semester.enum";
