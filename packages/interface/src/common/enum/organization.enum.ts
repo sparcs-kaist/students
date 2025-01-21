06db7a606014f1ed7c627287727c9bb62b44251c
Gerbera3090 <hw3090@gmail.com>
2025-01-21 20:58:18 +0900

    feat: add new organization enum

--- packages/interface/src/common/enum/organization.enum.ts
+++ packages/interface/src/common/enum/organization.enum.ts
@@ -1,11 +1,11 @@
 // 조직 유형 E
 export enum OrganizationTypeE {
+  UAPresident, // 학부 총학생회징딘
+  CentralExecutive, // 중앙집행위원회
   Autonomous = 1, // 자치기구
   Standing, // 상설위원회
   Specialized, // 전문기구
   Special, // 특별기구
-  Emergency, // 비상대책위원회
-  UA, // 학부 총학생회
   StudentCouncil, // 학과학생회
   Preparatory, // 준비위원회
   Affairs, // 특임위원회
@@ -18,11 +18,20 @@ export enum OrganizationPresidentTypeE {
   Vice, // 부
 }
 
+export enum OrganizationStateE {
+  Regular = 1, // 정규
+  Emergency, // 비대위
+}
+
 // OrganizationTypeE
 export const getDisplayNameOrganizationTypeE = (
   type: OrganizationTypeE | undefined,
 ) => {
   switch (type) {
+    case OrganizationTypeE.UAPresident:
+      return "총학생회장단";
+    case OrganizationTypeE.CentralExecutive:
+      return "중앙집행위원회";
     case OrganizationTypeE.Autonomous:
       return "자치기구";
     case OrganizationTypeE.Standing:
@@ -31,10 +40,6 @@ export const getDisplayNameOrganizationTypeE = (
       return "전문기구";
     case OrganizationTypeE.Special:
       return "특별기구";
-    case OrganizationTypeE.Emergency:
-      return "비상대책위원회";
-    case OrganizationTypeE.UA:
-      return "학부 총학생회";
     case OrganizationTypeE.StudentCouncil:
       return "학과학생회";
     case OrganizationTypeE.Preparatory:
