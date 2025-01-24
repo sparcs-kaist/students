1a59142d53df8c0837dbcc105340f9c9b943311a
malloc3141 <malloc_3141592@naver.com>
2025-01-24 13:55:30 +0900

    feat: remove SearchThreeInput

--- packages/web/src/features/proposal/components/SearchThreeInput.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import FlexWrapper from "@sparcs-students/web/common/components/FlexWrapper";
-
-const SearchThreeInput = () => <FlexWrapper direction="column" gap={16} />;
-
-export default SearchThreeInput;
