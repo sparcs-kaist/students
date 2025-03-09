efa6ec2b52850dd754c7c046ea45021f4789311c
malloc3141 <malloc_3141592@naver.com>
2025-03-09 19:38:55 +0900

    feat: impl budget report viewer page

--- packages/web/src/app/document-lookup/budget-report/result/[id]/page.tsx
+++ packages/web/src/app/document-lookup/budget-report/result/[id]/page.tsx
@@ -6,18 +6,16 @@ import FlexWrapper from "@sparcs-students/web/common/components/FlexWrapper";
 import Typography from "@sparcs-students/web/common/components/Typography";
 import Button from "@sparcs-students/web/common/components/Buttons/Button";
 import ViewResult from "@sparcs-students/web/features/documents/components/ViewResult";
-// import ViewerIncomeTable, {
-//   ViewerIncomeProps,
-// } from "@sparcs-students/web/features/budget/components/ViewerIncomeTable";
-// import ViewerExpenditureTable, {
-//   ViewerExpenditureProps,
-// } from "@sparcs-students/web/features/documents/components/ViewerExpenditureTable";
+import ViewerIncomeTable, {
+  ViewerIncomeProps,
+} from "@sparcs-students/web/features/budget/components/ViewerIncomeTable";
+import ViewerExpenditureTable, {
+  ViewerExpenditureProps,
+} from "@sparcs-students/web/features/documents/components/ViewerExpenditureTable";
 import TotalTable, {
   TotalProps,
 } from "@sparcs-students/web/features/documents/components/TotalTable";
 import {
-  mockExpenditureData,
-  mockIncomeData,
   mockViewerExpenditureData,
   mockViewerIncomeData,
   mockViewResultData,
@@ -28,17 +26,8 @@ import { mockData } from "@sparcs-students/web/features/documents/components/Thr
 import ThreeInput, {
   ThreeInputItem,
 } from "@sparcs-students/web/features/documents/components/ThreeInput";
-
-import ReviewerIncomeTable from "@sparcs-students/web/features/budget/components/ReviewerIncomeTable";
-import ReviewerExpenditureTable from "@sparcs-students/web/features/documents/components/ReviewerExpenditureTable";
-import { ViewerIncomeProps } from "@sparcs-students/web/features/budget/components/ViewerIncomeTable";
-import { ViewerExpenditureProps } from "@sparcs-students/web/features/documents/components/ViewerExpenditureTable";
-import styled from "styled-components";
-import { overlay } from "overlay-kit";
-import Modal from "@sparcs-students/web/common/components/Modal";
-import ConfirmModalContent from "@sparcs-students/web/common/components/Modal/ConfirmModalContent";
-import CancellableModalContent from "@sparcs-students/web/common/components/Modal/CancellableModalContent";
 import { BudgetDomainEnum } from "@sparcs-students/interface/common/enum/budget.enum";
+import { useParams } from "next/navigation";
 
 interface DomainAccum {
   incomeLastYear: number;
@@ -46,13 +35,6 @@ interface DomainAccum {
   expenditureLastYear: number;
   expenditureThisYear: number;
 }
-
-const ButtonWrapper = styled.div`
-  gap: 30px;
-  flex-direction: row;
-  display: flex;
-  justify-content: center;
-`;
 
 const dataToTotal = (
   incomeData: ViewerIncomeProps[],
@@ -155,7 +137,7 @@ const dataToTotal = (
 };
 
 const Proposal = () => {
-  // const { id } = useParams();
+  const { id } = useParams();
   const items: ThreeInputItem[] = mockData;
   const [date, setDate] = useState(mockViewResultData.submitDate);
   const [year, setYear] = useState<number>(items[0].year);
@@ -163,32 +145,6 @@ const Proposal = () => {
   const [type, setType] = useState<DocumentType>(DocumentType.BudgetProposal);
   const [selectedKey, setSelectedKey] = useState<string>(""); // TODO: enum으로 변경
   const [selectedValue, setSelectedValue] = useState<string>(""); // TODO: enum으로 변경
-  const userPermission = 2; // 1: viewer, 2: reviewer, 3: manager TODO: 실제 권한으로 변경
-
-  const openSaveModal = () => {
-    // TODO: add save logic
-    overlay.open(({ isOpen, close }) => (
-      <Modal isOpen={isOpen} width="400px">
-        <ConfirmModalContent onConfirm={() => close()}>
-          저장되었습니다.
-        </ConfirmModalContent>
-      </Modal>
-    ));
-  };
-
-  const openDiscardModal = () => {
-    // TODO: add discard logic
-    overlay.open(({ isOpen, close }) => (
-      <Modal isOpen={isOpen} width="400px">
-        <CancellableModalContent
-          onConfirm={() => close()}
-          onClose={() => close()}
-        >
-          임시저장 내역을{"\n"}모두 삭제하시겠습니까?
-        </CancellableModalContent>
-      </Modal>
-    ));
-  };
 
   return (
     <FlexWrapper direction="column" gap={48}>
@@ -222,36 +178,15 @@ const Proposal = () => {
           submitDate={date}
           handleDateChange={setDate}
         />
-        {/* {userPermission === 1 && <ViewerIncomeTable data={mockViewerIncomeData} />} */}
-        {userPermission === 2 && (
-          <ReviewerIncomeTable initialData={mockIncomeData} />
-        )}
-        {/* {userPermission === 3 && <ViewerIncomeTable data={mockViewerIncomeData} />} */}
-
-        {/* {userPermission === 1 && <ViewerExpenditureTable data={mockViewerExpenditureData} type="proposal" pageId={id} />} */}
-        {userPermission === 2 && (
-          <ReviewerExpenditureTable initialData={mockExpenditureData} />
-        )}
+        <ViewerIncomeTable data={mockViewerIncomeData} />
+        <ViewerExpenditureTable
+          data={mockViewerExpenditureData}
+          type="report"
+          pageId={id}
+        />
         <TotalTable
           data={dataToTotal(mockViewerIncomeData, mockViewerExpenditureData)}
         />
-        {userPermission === 2 && (
-          <ButtonWrapper>
-            <Button
-              type="reverse"
-              onClick={openDiscardModal}
-              style={{ width: "100px", padding: "8px 16px" }}
-            >
-              삭제
-            </Button>
-            <Button
-              onClick={openSaveModal}
-              style={{ width: "100px", padding: "8px 16px" }}
-            >
-              제출
-            </Button>
-          </ButtonWrapper>
-        )}
       </FlexWrapper>
     </FlexWrapper>
   );
