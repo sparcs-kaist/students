47a1f72a2b8648df268d130f83ac65d0cbd19684
Woojeong Choi <gldeel@kaist.ac.kr>
2025-03-09 23:54:29 +0900

    feat: impl TimeLineDateInput

---
+++ packages/web/src/features/project/components/_atomic/TimelineDateInput.tsx
@@ -0,0 +1,53 @@
+import DateInput from "@sparcs-students/web/common/components/DateInput";
+import FlexWrapper from "@sparcs-students/web/common/components/FlexWrapper";
+import Select from "@sparcs-students/web/common/components/Select";
+import { useState } from "react";
+
+const TimeLineDateInput = () => {
+  const [firstDate, setFirstDate] = useState<Date | null>(new Date());
+  const [secondDate, setSecondDate] = useState<Date | null>(new Date());
+
+  const [selectOption, setSelectOption] = useState<string | undefined>(
+    undefined,
+  );
+  return (
+    <FlexWrapper direction="row" gap={10}>
+      <div style={{ width: "180px" }}>
+        <Select
+          items={[
+            { label: "상반기", value: "상반기" },
+            { label: "하반기", value: "하반기" },
+            { label: "날짜", value: "날짜" },
+          ]}
+          value={selectOption}
+          onChange={option => setSelectOption(option)}
+          placeholder="유형을 선택하세요"
+        />
+      </div>
+      <div style={{ minWidth: "300px" }}>
+        <DateInput
+          selectsRange
+          disabled={selectOption !== "날짜"}
+          startDate={selectOption === "날짜" ? firstDate : null}
+          endDate={selectOption === "날짜" ? secondDate : null}
+          onChange={(dates: [Date | null, Date | null]) => {
+            const [start, end] = dates;
+            setFirstDate(start);
+            setSecondDate(end);
+          }}
+          selectedDates={
+            selectOption === "날짜"
+              ? [firstDate, secondDate].filter(
+                  (date): date is Date => date !== null,
+                )
+              : []
+          }
+          placeholderText="선택하세요"
+          dateFormat="yyyy.MM.dd"
+        />
+      </div>
+    </FlexWrapper>
+  );
+};
+
+export default TimeLineDateInput;
