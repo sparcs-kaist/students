b9082b7abfc369790af914b373dc7b781be4ce89
larrykwon <koallarry11@kaist.ac.kr>
2025-03-14 21:43:04 +0900

    feat: add cross-env and dotenv

--- env/dotenv-options.ts
+++ env/dotenv-options.ts
@@ -2,6 +2,7 @@ import path from "path";
 
 const env = process.env.NODE_ENV || "local";
 const envFilePath = path.join(process.cwd(), `env/.env.${env}`);
+console.log(envFilePath);
 const dotEnvOptions = {
   path: envFilePath,
 };
