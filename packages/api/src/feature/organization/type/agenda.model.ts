cf89d6e95a7a3c4ac41fe92578d7dbc68930255a
minjoo0729 <minjoo0729@kaist.ac.kr>
2025-03-18 23:17:17 +0900

    feat: models for agenda and meeting

---
+++ packages/api/src/feature/organization/type/agenda.model.ts
@@ -0,0 +1,28 @@
+import { IAgenda } from "@sparcs-students/interface/api/organization/type/meeting.type";
+import { InferSelectModel } from "drizzle-orm";
+
+import { Agenda } from "@sparcs-students/api/drizzle/schema/meeting.schema";
+
+export type AgendaDBResult = InferSelectModel<typeof Agenda>;
+
+export class MAgenda implements IAgenda {
+  id: IAgenda["id"];
+
+  meeting: IAgenda["meeting"];
+
+  accepted: IAgenda["accepted"];
+
+  constructor(data: IAgenda) {
+    Object.assign(this, data);
+  }
+
+  static fromDBResult(result: AgendaDBResult) {
+    return new MAgenda({
+      id: result.id,
+      meeting: {
+        id: result.meeting_id,
+      },
+      accepted: result.accepted,
+    });
+  }
+}
---
+++ packages/api/src/feature/organization/type/meeting.model.ts
@@ -0,0 +1,29 @@
+import { IMeeting } from "@sparcs-students/interface/api/organization/type/meeting.type";
+import { InferSelectModel } from "drizzle-orm";
+
+import { Meeting } from "@sparcs-students/api/drizzle/schema/meeting.schema";
+
+export type MeetingDBResult = InferSelectModel<typeof Meeting>;
+
+export class MMeeting implements IMeeting {
+  id: IMeeting["id"];
+
+  duration: IMeeting["duration"];
+
+  name: IMeeting["name"];
+
+  constructor(data: IMeeting) {
+    Object.assign(this, data);
+  }
+
+  static fromDBResult(result: MeetingDBResult) {
+    return new MMeeting({
+      id: result.id,
+      duration: {
+        startTerm: result.start_term,
+        endTerm: result.end_term,
+      },
+      name: result.name,
+    });
+  }
+}
