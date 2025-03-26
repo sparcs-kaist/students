46ba9b25bd6758e568caaccf31a6193d7fa74085
Chaeyeon Ahn <chacha@sparcs.org>
2025-03-26 12:22:14 +0900

    feat: add nav and connect notimplementedpage (#142)

---
+++ packages/web/src/features/NotImplemented/NotImplementedComponent.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+const NotImplementedComponent: React.FC = () => (
+  <div>
+    <h3>아직 구현되지 않았어요!</h3>
+  </div>
+);
+
+export default NotImplementedComponent;
