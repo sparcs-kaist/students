726f2a7e625d9927d97aa0b44a00f97f1f9bbfb2
oganessone718 <oganessone718@gmail.com>
2025-04-01 04:54:09 +0900

    fix: add groups editable table

---
+++ packages/web/src/features/project/components/ManageProjectReportTable.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+
+import FlexWrapper from "@sparcs-students/web/common/components/FlexWrapper";
+import Typography from "@sparcs-students/web/common/components/Typography";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import MemberEditableTable from "@sparcs-students/web/features/project/components/MemberEditableTable";
+import GroupsTag from "@sparcs-students/web/common/components/Tag/GroupsTag";
+
+export interface MemberProps {
+  id: string;
+  name: string;
+  groups: string[];
+}
+
+interface MemberEditTableProps {
+  data: MemberProps[];
+}
+
+const columnHelper = createColumnHelper<MemberProps>();
+
+const columns = [
+  columnHelper.accessor("id", {
+    id: "id",
+    header: "학번",
+    cell: info => info.getValue(),
+    size: 450,
+  }),
+  columnHelper.accessor("name", {
+    id: "name",
+    header: "이름",
+    cell: info => info.getValue(),
+    size: 450,
+  }),
+  columnHelper.accessor("groups", {
+    id: "groups",
+    header: "소속 부서",
+    cell: info => (
+      <div
+        style={{
+          display: "flex",
+          gap: "8px",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {info.getValue().length === 0 ? (
+          <GroupsTag color="GRAY" isDropdown={false}>
+            미소속
+          </GroupsTag>
+        ) : (
+          info.getValue().map((group, index) => (
+            <GroupsTag key={index} color="GREEN100" isDropdown={false}>
+              {group}
+            </GroupsTag>
+          ))
+        )}
+      </div>
+    ),
+    size: 975,
+  }),
+];
+
+const MemberEditTable: React.FC<MemberEditTableProps> = ({ data }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  // TODO: back 구현되면, 저장 버튼 클릭시시 editData로 data update하기
+  const [editData, setEditData] = useState<MemberProps[]>(data);
+
+  const table = useReactTable({
+    columns,
+    data: editData,
+    getCoreRowModel: getCoreRowModel(),
+    enableSorting: false,
+  });
+
+  return (
+    <FlexWrapper direction="column" gap={16}>
+      <FlexWrapper direction="row" gap={12}>
+        <Typography fs={20} lh={28} color="BLACK" fw="SEMIBOLD">
+          위원 명단
+        </Typography>
+      </FlexWrapper>
+      {loaded && (
+        <MemberEditableTable
+          table={table}
+          emptyMessage="운영위원 정보가 없습니다."
+          setEditData={setEditData}
+        />
+      )}
+    </FlexWrapper>
+  );
+};
+
+export default MemberEditTable;
---
+++ packages/web/src/features/project/components/MemberEditableTable.tsx
@@ -0,0 +1,345 @@
+import { useCallback, useState } from "react";
+
+import isPropValid from "@emotion/is-prop-valid";
+import {
+  flexRender,
+  Row,
+  type Table as TableType,
+} from "@tanstack/react-table";
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
+import Icon from "@sparcs-students/web/common/components/Icon";
+
+import GroupsTag from "@sparcs-students/web/common/components/Tag/GroupsTag";
+import TableCell from "@sparcs-students/web/common/components/Table/TableCell";
+import Typography from "@sparcs-students/web/common/components/Typography";
+
+interface TableProps<T> {
+  table: TableType<T>;
+  minWidth?: number;
+  height?: number;
+  emptyMessage?: string;
+  footer?: React.ReactNode;
+  rowLink?: (row: T) => string | { pathname: string };
+  onClick?: (row: T) => void;
+  rowStyleResolver?: (row: T) => React.CSSProperties;
+  setEditData?: React.Dispatch<React.SetStateAction<T[]>>;
+}
+const TableWrapper = styled.div`
+  width: 100%;
+  padding: 0;
+  //width: calc(100% + (100vw - 100%));
+  //padding: 0 calc((100vw - 100%) / 2);
+  overflow-x: auto;
+`;
+
+const TableInner = styled.table.withConfig({
+  shouldForwardProp: prop => isPropValid(prop),
+})<{ height?: number; minWidth: number }>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  min-width: ${({ minWidth }) => `max(100%, ${minWidth}px)`};
+  border: 1px solid ${({ theme }) => theme.colors.GRAY[100]};
+  border-radius: 4px;
+  overflow: hidden;
+  border-spacing: 0;
+  /* height: ${({ height }) => (height ? `${height}px` : "fit-content")}; */
+  table-layout: fixed;
+  width: 100%;
+`;
+const Header = styled.thead`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 100%;
+`;
+const HeaderRow = styled.tr`
+  display: flex;
+  width: 100%;
+`;
+const Content = styled.tbody`
+  display: block;
+  flex: 1;
+  // overflow: hidden;
+  width: 100%;
+  height: fit-content;
+`;
+
+const ContentRow = styled.tr.withConfig({
+  shouldForwardProp: prop => isPropValid(prop),
+})<{ selected: boolean; isClickable: boolean }>`
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.GRAY[100]};
+  cursor: ${({ isClickable }) => (isClickable ? "pointer" : "default")};
+  background-color: ${({ selected, theme }) =>
+    selected ? theme.colors.GREEN[100] : "transparent"};
+  /* overflow: hidden; */
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  height: fit-content;
+`;
+
+const EmptyCenterPlacer = styled.div`
+  height: 100%;
+  display: flex;
+  flex: 1;
+  padding: 12px 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DropdownHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Dropdown = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  width: 400px;
+  padding: 12px;
+  gap: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.GRAY[100]};
+  border-top: none;
+  border-radius: 4px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  z-index: 5;
+  transform: translateX(20px) translateY(12px);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const GroupDropdownCell = <T extends { id: string; groups: string[] }>({
+  row,
+  setEditData,
+}: {
+  row: Row<T>;
+  setEditData: React.Dispatch<React.SetStateAction<T[]>>;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownOpen = () => {
+    setIsOpen(prev => !prev);
+  };
+  // 임시 부서 이름 목록
+  const testGroups = [
+    "부서 1",
+    "부서 2",
+    "이름이 굉장히 긴 어떤 부서 예시",
+    "부서 3",
+    "부서 4",
+    "부서 5",
+    "부서 6",
+  ];
+
+  // TODO: 임시로 부서 이름 스트링을 기준으로 구현했는데, 추후 백엔드 구현 방식에 따라 id 이용해서 구현하는 게 나을 듯. 그때 되면 타입도 수정.
+  const groupClick = (selectedGroup: string) => {
+    setEditData(prev =>
+      prev.map(member => {
+        console.log(member.id, row.original.id);
+        if (member.id !== row.original.id) return member;
+        if (selectedGroup === "미소속") {
+          return {
+            ...member,
+            groups: [],
+          };
+        }
+        return {
+          ...member,
+          groups: member.groups.includes(selectedGroup)
+            ? member.groups.filter((group: string) => group !== selectedGroup)
+            : [...member.groups, selectedGroup],
+        };
+      }),
+    );
+  };
+
+  return (
+    <>
+      <DropdownHeader onClick={dropdownOpen} style={{ cursor: "pointer" }}>
+        <Icon
+          type={isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+          size={20}
+        />
+      </DropdownHeader>
+      {isOpen && (
+        <Dropdown>
+          {testGroups.map((group, index) => (
+            <GroupsTag
+              key={index}
+              color="GREEN100"
+              onClick={() => {
+                groupClick(group);
+              }}
+              isDropdown
+            >
+              {group}
+            </GroupsTag>
+          ))}
+          <GroupsTag
+            color="GRAY"
+            onClick={() => {
+              groupClick("미소속");
+            }}
+            isDropdown
+          >
+            미소속
+          </GroupsTag>
+        </Dropdown>
+      )}
+    </>
+  );
+};
+
+const MemberEditableTable = <T extends { id: string; groups: string[] }>({
+  table,
+  minWidth = 400,
+  height = undefined,
+  emptyMessage = "내역이 없습니다",
+  footer = null,
+  rowLink = undefined,
+  onClick = undefined,
+  rowStyleResolver = undefined,
+  setEditData = undefined,
+}: TableProps<T>) => {
+  // 야매로 min-width 바꿔치기 (고치지 마세요)
+  // eslint-disable-next-line no-underscore-dangle
+  table._getColumnDefs().forEach(column => {
+    // eslint-disable-next-line no-param-reassign
+    column.minSize = 0;
+  });
+  const router = useRouter();
+  const handleRowClick = useCallback(
+    (row: T) => {
+      if (rowLink) {
+        const link = rowLink(row);
+        if (link) {
+          if (typeof link === "string") {
+            router.push(link);
+          } else if (link?.pathname) {
+            router.push(link.pathname);
+          }
+        }
+      }
+      if (onClick) {
+        onClick(row);
+      }
+    },
+    [rowLink, onClick, router],
+  );
+
+  return (
+    <TableWrapper>
+      <TableInner height={height} minWidth={minWidth}>
+        <Header>
+          {table.getHeaderGroups().map(headerGroup => (
+            <HeaderRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                if (header.isPlaceholder) {
+                  return null;
+                }
+                if (header.column.getCanSort()) {
+                  return (
+                    <TableCell
+                      key={header.id}
+                      width={header.column.getSize()}
+                      type="HeaderSort"
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </TableCell>
+                  );
+                }
+                return (
+                  <TableCell
+                    key={header.id}
+                    width={header.column.getSize()}
+                    type="Header"
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </TableCell>
+                );
+              })}
+            </HeaderRow>
+          ))}
+        </Header>
+        <Content>
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map(row => {
+              // 표 스타일 디자인과 맞추기
+              const style = rowStyleResolver
+                ? rowStyleResolver(row.original)
+                : undefined;
+              return (
+                <ContentRow
+                  key={row.id}
+                  selected={row.getIsSelected()}
+                  isClickable={!!rowLink || !!onClick}
+                  onClick={() => handleRowClick(row.original)}
+                  style={style}
+                >
+                  {row.getVisibleCells().map(cell =>
+                    cell.column.columnDef.header === "소속 부서" ? (
+                      <TableCell
+                        key={cell.id}
+                        width={cell.column.getSize()}
+                        type="Icon"
+                        icon={
+                          <GroupDropdownCell
+                            row={row}
+                            setEditData={setEditData!}
+                          />
+                        }
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ) : (
+                      <TableCell
+                        key={cell.id}
+                        width={cell.column.getSize()}
+                        type="Default"
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ),
+                  )}
+                </ContentRow>
+              );
+            })
+          ) : (
+            <EmptyCenterPlacer>
+              <Typography
+                fs={16}
+                lh={24}
+                color="GRAY.200"
+                ff="PRETENDARD"
+                fw="REGULAR"
+              >
+                {emptyMessage}
+              </Typography>
+            </EmptyCenterPlacer>
+          )}
+          {footer}
+        </Content>
+      </TableInner>
+    </TableWrapper>
+  );
+};
+export default MemberEditableTable;
