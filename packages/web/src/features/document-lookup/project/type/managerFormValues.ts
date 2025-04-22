8e410dff8bf866dafeac15f6cc8cd81395620cd0
chacha <chacha@sparcs.org>
2025-04-23 02:09:13 +0900

    feat: create mock data for budgetProposal and projectProposal

---
+++ packages/web/src/features/document-lookup/project/type/managerFormValues.ts
@@ -0,0 +1,36 @@
+import { TimelineDateTypeEnum } from "@sparcs-students/web/features/project/services/_mock/mockProjectTimelineData";
+
+export enum DocumentTypeEnum {
+  BudgetProposal = 1,
+  BudgetReport,
+  ProjectProposal,
+  ProjectReport,
+}
+
+export interface ProjectDate {
+  value: [Date | null, Date | null];
+  type: TimelineDateTypeEnum | undefined;
+}
+
+// 사업 계획서와 사업 계획서 상세 페이지에 들어가는 정보
+export interface ProjectProposalContent {
+  id: number; // id
+  contentId: number; // 같은 documentUniqueId 내의 여러 상세 페이지 중의 id
+  title: string; // 사업명
+  brief: string; // 사업 개요
+  preparationPeriod: ProjectDate; // 사업 준비 기간
+  executionPeriod: ProjectDate; // 사업 일시
+  teamId: number; // 담당 부서 id
+  manager: number; // 담당자 userId
+  purpose: string; // 사업 추진 목적
+  beneficiary: string; // 사업 수혜 대상자
+  detail: string; // 세부 사업 내용
+  timelineIds: number[]; // 사업 진행 타임라인 id의 배열
+  expenditure: number[]; // 사업 계획서에 편성된 예산안 row의 id 배열
+}
+
+export interface ProjectProposal {
+  documentUniqueId: number; // 분기, 문서 유형, 단체 조합의 고유한 id
+  documentTypeEnumId: DocumentTypeEnum; // 문서 4개 중 1개
+  contentIds: number[]; // ProjectProposalContentId의 배열
+}
