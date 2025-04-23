5eabcac6ab4fc2f862d4b67f04b5f6ca75c61789
larrykwon <koallarry11@kaist.ac.kr>
2025-04-23 23:20:21 +0900

    feat: find user with uid, not email

--- packages/api/src/feature/auth/repository/auth.repository.ts
+++ packages/api/src/feature/auth/repository/auth.repository.ts
@@ -39,7 +39,7 @@ export class AuthRepository {
     const user = await this.db
       .select()
       .from(User)
-      .where(eq(User.email, uid))
+      .where(eq(User.uid, uid))
       .then(takeOne);
 
     let result: MemberDbResult = {
