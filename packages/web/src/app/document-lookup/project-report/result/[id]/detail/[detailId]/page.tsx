cd308b3cd89b3bbea86839ba2dd3485806d158b2
Chaeyeon Ahn <chacha@sparcs.org>
2025-04-23 14:25:27 +0900

    feat: create mock data for budgetProposal and projectProposal (#174)

--- packages/web/src/app/document-lookup/project-report/result/[id]/detail/[detailId]/page.tsx
+++ packages/web/src/app/document-lookup/project-report/result/[id]/detail/[detailId]/page.tsx
@@ -9,7 +9,7 @@ import Index from "@sparcs-students/web/common/components/Index";
 import Typography from "@sparcs-students/web/common/components/Typography";
 import BudgetReportTable from "@sparcs-students/web/features/document-lookup/project/components/BudgetReportTable";
 import DocumentTimelineTable from "@sparcs-students/web/features/document-lookup/project/components/DocumentTimelineTable";
-import mockBudgetReportData from "@sparcs-students/web/features/document-lookup/project/services/_mock/mockBudgetReportTable";
+import mockBudgetReportData from "@sparcs-students/web/features/document-lookup/budget/services/_mock/mockBudgetReportTable";
 import { mockDocumentTimelineData } from "@sparcs-students/web/features/document-lookup/project/services/_mock/mockDocumentTimelineTable";
 import TextAreaWithHeader from "@sparcs-students/web/features/report/components/TextAreaWithHeader";
 
