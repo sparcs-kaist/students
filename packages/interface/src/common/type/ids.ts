b50f414ebdd21efe57007416ade771744954c6e3
Gerbera3090 <hw3090@gmail.com>
2025-04-28 03:29:17 +0900

    feat: extract id for interface util

--- packages/interface/src/common/type/ids.ts
+++ packages/interface/src/common/type/ids.ts
@@ -1,3 +1,11 @@
 import z from "zod";
 
 export const zId = z.coerce.number().int().min(1);
+
+export type ExtractId<T extends z.ZodObject<{ id: typeof zId }>> = z.ZodObject<{
+  id: T["shape"]["id"];
+}>;
+
+export const zExtractId = <T extends z.ZodObject<{ id: typeof zId }>>(
+  schema: T,
+): ExtractId<T> => z.object({ id: schema.shape.id }) as ExtractId<T>;
