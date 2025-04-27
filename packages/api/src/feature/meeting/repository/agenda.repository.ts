136f426f4af6b0080683a16f81d46697ee32ed52
Gerbera3090 <hw3090@gmail.com>
2025-04-28 04:48:18 +0900

    feat: repository and module for meeting and agenda

---
+++ packages/api/src/feature/meeting/repository/agenda.repository.ts
@@ -0,0 +1,99 @@
+import { Injectable } from "@nestjs/common";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+import {
+  BaseTableFieldMapKeys,
+  TableWithID,
+} from "@sparcs-students/api/common/base/base.repository";
+import { BaseSingleTableRepository } from "@sparcs-students/api/common/base/base.single.repository";
+import { AgendaStatusEnum } from "@sparcs-students/interface/api/meeting/type/agenda.type";
+import { EmptyObject } from "@sparcs-students/api/common/base/entity.model";
+import { Agenda } from "src/drizzle/schema";
+
+import { IAgendaCreate, MAgenda } from "../model/agenda.model";
+
+export type AgendaQuery = {
+  name: string;
+  meetingId: number;
+  agendaStatusEnum: AgendaStatusEnum;
+};
+
+type AgendaOrderByKeys = "id" | "agendaStatusEnum";
+type AgendaQuerySupport = EmptyObject;
+
+type AgendaTable = typeof Agenda;
+type AgendaDbSelect = InferSelectModel<AgendaTable>;
+type AgendaDbUpdate = Partial<AgendaDbSelect>;
+type AgendaDbInsert = InferInsertModel<AgendaTable>;
+type AgendaFieldMapKeys = BaseTableFieldMapKeys<
+  AgendaQuery,
+  AgendaOrderByKeys,
+  AgendaQuerySupport
+>;
+
+@Injectable()
+export class AgendaRepository extends BaseSingleTableRepository<
+  MAgenda,
+  IAgendaCreate,
+  AgendaTable,
+  AgendaQuery,
+  AgendaOrderByKeys,
+  AgendaQuerySupport
+> {
+  constructor() {
+    super(Agenda, MAgenda);
+  }
+
+  protected dbToModelMapping(result: AgendaDbSelect): MAgenda {
+    return new MAgenda({
+      id: result.id,
+      meeting: { id: result.meetingId },
+      agendaTypeEnum: result.agendaTypeEnum,
+      name: result.name,
+      detail: result.detail,
+      agendaStatusEnum: result.agendaStatusEnum,
+      submittedAt: result.submittedAt,
+    });
+  }
+
+  protected modelToDBMapping(model: MAgenda): AgendaDbUpdate {
+    return {
+      id: model.id,
+      meetingId: model.meeting.id,
+      name: model.name,
+      detail: model.detail,
+      agendaTypeEnum: model.agendaTypeEnum,
+      agendaStatusEnum: model.agendaStatusEnum,
+      submittedAt: model.submittedAt,
+    };
+  }
+
+  protected createToDBMapping(model: IAgendaCreate): AgendaDbInsert {
+    return {
+      meetingId: model.meeting.id,
+      name: model.name,
+      detail: model.detail,
+      agendaTypeEnum: model.agendaTypeEnum,
+      agendaStatusEnum: model.agendaStatusEnum,
+    };
+  }
+
+  protected fieldMap(
+    field: AgendaFieldMapKeys,
+  ): TableWithID | null | undefined {
+    const fieldMappings: Record<AgendaFieldMapKeys, TableWithID | null> = {
+      id: Agenda,
+      meetingId: Agenda,
+      agendaStatusEnum: Agenda,
+      submittedAt: Agenda,
+      name: Agenda,
+      detail: Agenda,
+    };
+
+    if (!(field in fieldMappings)) {
+      return undefined;
+    }
+
+    return fieldMappings[field];
+  }
+}
