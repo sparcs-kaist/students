d6dc8550d2bb8a2ecbefabb78e27d77bf7d42e7d
chacha <chacha@sparcs.org>
2025-04-30 01:22:06 +0900

    feat: change gyeolsanan to gyeolsan

--- packages/web/src/app/document-lookup/budget-report/result/[id]/page.tsx
+++ packages/web/src/app/document-lookup/budget-report/result/[id]/page.tsx
@@ -63,7 +63,7 @@ const Report = () => {
       case "예산안":
         router.push(`/document-lookup/budget-proposal/result/${id}?${query}`);
         break;
-      case "결산안":
+      case "결산":
         router.push(`/document-lookup/budget-report/result/${id}?${query}`);
         break;
       default:
