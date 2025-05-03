beb5bbcbd3d645e58c24b7d15ca961963694bbda
Woojeong Choi <gldeel@kaist.ac.kr>
2025-05-03 21:31:42 +0900

    feat: content

---
+++ packages/web/src/features/info/components/Content.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import isPropValid from "@emotion/is-prop-valid";
+import styled, { DefaultTheme } from "styled-components";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+interface ContentProps {
+  content: string;
+  withArrow: boolean;
+  isSubContent: boolean;
+  selected: boolean;
+}
+
+const getTextColor = (props: {
+  theme: DefaultTheme;
+  selected: boolean;
+  isSubContent: boolean;
+}) => {
+  const { theme, selected, isSubContent } = props;
+
+  if (selected && !isSubContent) return theme.colors.WHITE;
+
+  if (selected && isSubContent) return theme.colors.PRIMARY;
+
+  return theme.colors.BLACK;
+};
+
+const ContentWrapper = styled.div.withConfig({
+  shouldForwardProp: prop => isPropValid(prop),
+})<{
+  isSubContent: boolean;
+  selected: boolean;
+}>`
+  width: 240px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+
+  background-color: ${({ theme, selected, isSubContent }) =>
+    selected && !isSubContent && theme.colors.PRIMARY};
+  border-left: ${({ theme, isSubContent }) =>
+    isSubContent && `1px solid ${theme.colors.GRAY[200]}`};
+  border-right: ${({ theme, isSubContent }) =>
+    isSubContent && `1px solid ${theme.colors.GRAY[200]}`};
+  border: ${({ theme, isSubContent }) =>
+    !isSubContent && `1px solid ${theme.colors.PRIMARY}`};
+  padding: ${({ isSubContent }) => (isSubContent ? "12px 48px" : "12px 24px")};
+  gap: 10px;
+
+  color: ${getTextColor};
+  font-weight: ${({ theme, selected }) =>
+    selected && theme.fonts.WEIGHT.SEMIBOLD};
+`;
+
+const Content: React.FC<ContentProps> = ({
+  content,
+  withArrow,
+  isSubContent,
+  selected,
+}) => (
+  <ContentWrapper isSubContent={isSubContent} selected={selected}>
+    {content}
+    {withArrow &&
+      !isSubContent &&
+      (selected ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />)}
+  </ContentWrapper>
+);
+
+export default Content;
