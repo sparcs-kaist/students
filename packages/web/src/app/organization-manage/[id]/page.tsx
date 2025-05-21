267ad5f2b1d4c7fa75615eab3886ab56948cff99
malloc3141 <malloc_3141592@naver.com>
2025-05-21 13:48:05 +0900

    chore: move page tsx

--- packages/web/src/app/organization-manage/[id]/page.tsx
+++ packages/web/src/app/organization-manage/[id]/page.tsx
@@ -10,6 +10,7 @@ import { useForm } from "react-hook-form";
 import { mockOrganizationMemberData } from "@sparcs-students/web/features/organization-manage/services/_mock/mockOrganizationManageData";
 
 const OrganizationManage = () => {
+  // TODO: remove esline disable (임시라서 넣어놓음)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [dirtyMemberData, setDirtyMemberData] = useState<{
     updatedRows: OrganizationMemberProps[];
