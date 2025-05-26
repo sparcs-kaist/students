a233275136eeb254097cef8f57c9f251981053a6
minjoo0729 <minjoo0729@kaist.ac.kr>
2025-05-26 19:17:06 +0900

    fix: project proposal controller

--- packages/api/src/projectProposalController.ts
+++ packages/api/src/projectProposalController.ts
@@ -10,7 +10,6 @@ import {
   HttpCode,
   HttpStatus,
 } from "@nestjs/common";
-// import { ProjectProposalService } from './projectProposalService';
 import { ApiPrp002RequestParam } from "@sparcs-students/interface/api/proposal/endpoint/apiPrp002";
 import { ApiPrp004RequestBody } from "@sparcs-students/interface/api/proposal/endpoint/apiPrp004";
 import {
@@ -22,25 +21,23 @@ import {
   ApiPrp006RequestBody,
 } from "@sparcs-students/interface/api/proposal/endpoint/apiPrp006";
 import { ApiPrp007RequestParam } from "@sparcs-students/interface/api/proposal/endpoint/apiPrp007";
+import { ProjectProposalService } from "./projectProposalService";
 
 @Controller("proposals/project-proposals")
 export class ProjectProposalController {
   constructor(private readonly service: ProjectProposalService) {}
 
-  /** PRP-002: 사업계획서 상세 조회 */
   @Get("project-proposal/:projectProposalId")
   getProposal(@Param() params: ApiPrp002RequestParam) {
     return this.service.getProposal(params.projectProposalId);
   }
 
-  /** PRP-004: 사업계획서 생성 */
   @Post("project-proposal")
   @HttpCode(HttpStatus.CREATED)
   createProposal(@Body() body: ApiPrp004RequestBody) {
     return this.service.createProposal(body);
   }
 
-  /** PRP-005: 사업계획서 수정 */
   @Put("project-proposal/:projectProposalId")
   updateProposal(
     @Param() params: ApiPrp005RequestParam,
@@ -49,16 +46,14 @@ export class ProjectProposalController {
     return this.service.updateProposal(params.projectProposalId, body);
   }
 
-  /** PRP-006: 사업계획서 제출 */
   @Put("project-proposal/submit")
   submitProposal(
-    @Body() body: ApiPrp006RequestBody,
     @Query() params: ApiPrp006RequestParam,
+    @Body() body: ApiPrp006RequestBody,
   ) {
     return this.service.submitProposal(params.projectProposalId, body);
   }
 
-  /** PRP-007: 사업계획서 삭제 */
   @Delete("project-proposal/:projectProposalId")
   deleteProposal(@Param() params: ApiPrp007RequestParam) {
     return this.service.deleteProposal(params.projectProposalId);
--- packages/interface/src/api/proposal/index.ts
+++ packages/interface/src/api/proposal/index.ts
@@ -5,10 +5,13 @@ export * from "./endpoint/apiPrp002";
 export { default as apiPrp002 } from "./endpoint/apiPrp002";
 
 export * from "./endpoint/apiPrp004";
-export { default as apiPrp004 } from "./endpoint/apiPrp004";
+// export { default as apiPrp004 } from "./endpoint/apiPrp004";
 
 export * from "./endpoint/apiPrp005";
-export { default as apiPrp005 } from "./endpoint/apiPrp005";
+// export { default as apiPrp005 } from "./endpoint/apiPrp005";
 
 export * from "./endpoint/apiPrp006";
-export { default as apiPrp006 } from "./endpoint/apiPrp006";
+// export { default as apiPrp006 } from "./endpoint/apiPrp006";
+
+export * from "./endpoint/apiPrp007";
+// export { default as apiPrp007 } from "./endpoint/apiPrp007";
