7bcd06f67b38073c91367d440e4ebe02fb2899cd
oganessone718 <oganessone718@gmail.com>
2025-05-28 21:05:50 +0900

    fix: add mock data

--- packages/web/src/features/organization-manage/services/_mock/mockOrganizationManageData.ts
+++ packages/web/src/features/organization-manage/services/_mock/mockOrganizationManageData.ts
@@ -1,5 +1,6 @@
 import { OrganizationMemberProps } from "@sparcs-students/web/features/organization-manage/components/ManageMemberTable";
 import { MemberRoleEnum } from "@sparcs-students/root/packages/interface/src/common/enum/organization.enum";
+import { CommitteeProps } from "@sparcs-students/web/features/organization-manage/components/ManageCommitteeTable";
 
 export const mockOrganizationMemberData: OrganizationMemberProps[] = [
   {
@@ -25,5 +26,26 @@ export const mockOrganizationMemberData: OrganizationMemberProps[] = [
     role: MemberRoleEnum.Member,
     startDate: "2025-05-22",
     endDate: "2025-12-31",
+  },
+];
+
+export const mockCommitteeListData: CommitteeProps[] = [
+  {
+    id: "1",
+    name: "간식이벤트",
+    leader: "박정민",
+    headcount: 5,
+  },
+  {
+    id: "2",
+    name: "스튜던츠",
+    leader: "김스튜",
+    headcount: 7,
+  },
+  {
+    id: "3",
+    name: "스팍스",
+    leader: "김팍스",
+    headcount: 18,
   },
 ];
