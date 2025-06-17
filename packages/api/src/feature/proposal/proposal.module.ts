213f4995ccd68bdab3f9ca220a56457a9a143fc7
Gerbera3090 <hw3090@gmail.com>
2025-06-17 10:55:47 +0900

    fix: change operation proposal entity

--- packages/api/src/feature/proposal/proposal.module.ts
+++ packages/api/src/feature/proposal/proposal.module.ts
@@ -3,13 +3,13 @@ import { DrizzleModule } from "@sparcs-students/api/drizzle/drizzle.module";
 import { BudgetProposalExpenseRepository } from "./repository/budget-proposal-expense.repository";
 import { BudgetProposalExpenseRevisionRepository } from "./repository/budget-proposal-expense-revision.repository";
 import { BudgetProposalIncomeRepository } from "./repository/budget-proposal-income.repository";
-import { OperatingCommitteeProposalRepository } from "./repository/operating-committee-proposal.repository";
 import { OperationProposalRepository } from "./repository/operation-proposal.repository";
 import { ProjectProposalRepository } from "./repository/project-proposal.repository";
-import { OperatingCommitteeProposalRevisionRepository } from "./repository/operating-committee-proposal-revision.repository";
 import { BudgetProposalIncomeRevisionRepository } from "./repository/budget-proposal-income-revision.repository";
 import { OperationProposalRevisionRepository } from "./repository/operation-proposal-revision.repository";
 import { ProjectProposalRevisionRepository } from "./repository/project-proposal-revision.repository";
+import { ProposalController } from "./controller/proposal.controller";
+import { ProposalService } from "./service/proposal.service";
 
 @Module({
   providers: [
@@ -19,11 +19,19 @@ import { ProjectProposalRevisionRepository } from "./repository/project-proposal
     BudgetProposalExpenseRevisionRepository,
     BudgetProposalIncomeRepository,
     BudgetProposalIncomeRevisionRepository,
-    OperatingCommitteeProposalRepository,
-    OperatingCommitteeProposalRevisionRepository,
     OperationProposalRepository,
     OperationProposalRevisionRepository,
+    ProposalService,
   ],
   imports: [DrizzleModule],
+  exports: [
+    ProjectProposalRepository,
+    ProjectProposalRevisionRepository,
+    BudgetProposalExpenseRepository,
+    BudgetProposalExpenseRevisionRepository,
+    BudgetProposalIncomeRepository,
+    BudgetProposalIncomeRevisionRepository,
+  ],
+  controllers: [ProposalController],
 })
 export class ProposalModule {}
