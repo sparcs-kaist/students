2ea05d090f91ffdab14a2bd3ba44e64f19d1537f
malloc3141 <malloc_3141592@naver.com>
2025-09-25 19:43:30 +0900

    fix: fix conflict

--- packages/web/src/features/organization-manage/services/_mock/mockOrganizationManageData.ts
+++ packages/web/src/features/organization-manage/services/_mock/mockOrganizationManageData.ts
@@ -3,7 +3,6 @@ import { CommitteeProps } from "@sparcs-students/web/features/organization-manag
 import {
   MemberRoleEnum,
   CommitteeRoleEnum,
-  DepartmentRoleEnum,
 } from "@sparcs-students/root/packages/interface/src/common/enum/organization.enum";
 
 export const mockOrganizationMemberData: OrganizationMemberProps[] = [
@@ -96,17 +95,71 @@ export const mockCommitteeMemberData: OrganizationMemberProps[] = [
   },
 ];
 
-export const mockDepartmentMemberData: OrganizationMemberProps[] = [
+export const mockAnotherCommitteeMemberData: OrganizationMemberProps[] = [
   {
     id: 1,
     studentId: "20210000",
     name: "박정민",
-    role: DepartmentRoleEnum.Chief,
+    role: CommitteeRoleEnum.Chief,
+    startDate: "2025-05-22",
+    endDate: "2025-12-31",
   },
   {
     id: 2,
     studentId: "20210000",
     name: "박정민",
-    role: DepartmentRoleEnum.Member,
+    role: CommitteeRoleEnum.Member,
+    startDate: "2025-05-22",
+    endDate: "2025-12-31",
+  },
+  {
+    id: 3,
+    studentId: "20210000",
+    name: "박정민",
+    role: CommitteeRoleEnum.Member,
+    startDate: "2025-05-22",
+    endDate: "2025-12-31",
+  },
+];
+
+export type OrganizationMemberTableType = {
+  id: number;
+  name: string;
+  OrganizationMember: OrganizationMemberProps[];
+};
+
+export const mockCommitteeMemberTableData: OrganizationMemberTableType[] = [
+  { id: 1, name: "운영위원회", OrganizationMember: mockCommitteeMemberData },
+  {
+    id: 2,
+    name: "확대운영위원회",
+    OrganizationMember: mockAnotherCommitteeMemberData,
+  },
+];
+
+export const mockSearchMemberData: OrganizationMemberProps[] = [
+  {
+    id: 7,
+    studentId: "20210000",
+    name: "박정민",
+    role: MemberRoleEnum.Chief,
+    startDate: "2025-07-18",
+    endDate: "2025-12-31",
+  },
+  {
+    id: 18,
+    studentId: "20210000",
+    name: "빅정민",
+    role: MemberRoleEnum.Vice,
+    startDate: "2025-05-22",
+    endDate: "2025-07-18",
+  },
+  {
+    id: 718,
+    studentId: "20210000",
+    name: "박민정",
+    role: MemberRoleEnum.Member,
+    startDate: "2024-07-18",
+    endDate: "2025-07-18",
   },
 ];
