821fac8329ab27a311e502b2d82ccfb16abf09db
송진형 <roysong0203@gmail.com>
2025-10-02 15:18:49 +0900

    feat: add dropdown to change page size in notice page

--- packages/web/src/app/notice/page.tsx
+++ packages/web/src/app/notice/page.tsx
@@ -10,6 +10,7 @@ import Icon from "@sparcs-students/web/common/components/Icon";
 import TextInput from "@sparcs-students/web/common/components/Forms/TextInput";
 import ModalTableButton from "@sparcs-students/web/common/components/Buttons/ModalTableButton";
 import styled from "styled-components";
+import Select from "@sparcs-students/web/common/components/Selects/Select";
 // import colors from "@sparcs-students/web/styles/themes/colors";
 
 interface RowProps {
@@ -60,6 +61,16 @@ const SearchBarWrapper = styled.div`
   max-width: 408px;
 `;
 
+const PageSizeSettingWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 151px;
+  min-width: 151px;
+  max-width: 151px;
+`;
+
 const SearchBar = ({
   handleSearch,
 }: {
@@ -88,34 +99,61 @@ const SearchBar = ({
   );
 };
 
-// const PageSetting = () => {
-// 	return (
-// 		<FlexWrapper direction="row" gap={10} justify="flex-start" alignItems="center">
-// 			<ModalTableButton type="reverse">
-// 				<Icon type="sort" size={28} color={colors.GREEN[700]} />
-// 			</ModalTableButton>
-// 		</FlexWrapper>
-// 	);
-// };
+const PageSizeSetting = ({
+  handlePageSizeChange,
+}: {
+  handlePageSizeChange: (newPageSize: number) => void;
+}) => {
+  const PageSizeList = [5, 10, 15, 20, 30, 50, 100];
+  const PageSizeItems = PageSizeList.map(size => ({
+    label: size.toString().concat("건"),
+    value: size.toString(),
+  }));
+
+  const [pageSize, setPageSize] = useState(10);
+
+  return (
+    <PageSizeSettingWrapper>
+      <Select
+        items={PageSizeItems}
+        value={pageSize.toString()}
+        onChange={newSize => {
+          const size = Number(newSize);
+          setPageSize(size);
+          handlePageSizeChange(size);
+        }}
+      />
+    </PageSizeSettingWrapper>
+  );
+};
 
 const SmallFrame = ({
   handleSearch,
+  handlePageSizeChange,
 }: {
   handleSearch: (searchText: string) => void;
+  handlePageSizeChange: (newPageSize: number) => void;
 }) => (
   <FlexWrapper direction="column" gap={20}>
     <SearchBar handleSearch={handleSearch} />
-    {/* <PageSetting /> */}
+    <PageSizeSetting handlePageSizeChange={handlePageSizeChange} />
   </FlexWrapper>
 );
 
 const LargeFrame = ({
   handleSearch,
+  handlePageSizeChange,
 }: {
   handleSearch: (searchText: string) => void;
+  handlePageSizeChange: (newPageSize: number) => void;
 }) => (
-  <FlexWrapper direction="row" gap={20} justify="flex-end" alignItems="center">
-    {/* <PageSetting /> */}
+  <FlexWrapper
+    direction="row"
+    gap={20}
+    justify="space-between"
+    alignItems="center"
+  >
+    <PageSizeSetting handlePageSizeChange={handlePageSizeChange} />
     <SearchBarWrapper>
       <SearchBar handleSearch={handleSearch} />
     </SearchBarWrapper>
@@ -126,13 +164,14 @@ const Notice = () => {
   // const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
   const [pageIndex, setPageIndex] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
   const [searchedNotice, setSearchedNotice] = useState<RowProps[]>(allNotice);
   const [shownNotice, setShownNotice] = useState<RowProps[]>([]);
 
   const handlePageChange = (newPageIndex: number) => {
     setPageIndex(newPageIndex);
-    const start = (newPageIndex - 1) * 10;
-    const end = start + 10;
+    const start = (newPageIndex - 1) * pageSize;
+    const end = start + pageSize;
     setShownNotice(allNotice.slice(start, end));
   };
 
@@ -142,7 +181,13 @@ const Notice = () => {
     );
     setSearchedNotice(filtered);
     setPageIndex(1);
-    setShownNotice(filtered.slice(0, 10));
+    setShownNotice(filtered.slice(0, pageSize));
+  };
+
+  const handlePageSizeChange = (newPageSize: number) => {
+    setPageSize(newPageSize);
+    setPageIndex(1);
+    setShownNotice(searchedNotice.slice(0, newPageSize));
   };
 
   useEffect(() => {
@@ -170,8 +215,8 @@ const Notice = () => {
 
   useEffect(() => {
     setSearchedNotice(allNotice);
-    const start = (pageIndex - 1) * 10;
-    const end = start + 10;
+    const start = (pageIndex - 1) * pageSize;
+    const end = start + pageSize;
     setShownNotice(allNotice.slice(start, end));
   }, []);
 
@@ -182,8 +227,18 @@ const Notice = () => {
         <BreadCrumb items={[{ name: "공지사항", path: "/notice" }]} />
       </FlexWrapper>
       <FlexWrapper direction="column" gap={20}>
-        {isMobile && <SmallFrame handleSearch={handleSearch} />}
-        {!isMobile && <LargeFrame handleSearch={handleSearch} />}
+        {isMobile && (
+          <SmallFrame
+            handleSearch={handleSearch}
+            handlePageSizeChange={handlePageSizeChange}
+          />
+        )}
+        {!isMobile && (
+          <LargeFrame
+            handleSearch={handleSearch}
+            handlePageSizeChange={handlePageSizeChange}
+          />
+        )}
         <SingleColumnTable
           header={`총 ${searchedNotice.length}건`}
           rows={shownNotice}
@@ -193,7 +248,7 @@ const Notice = () => {
         <Pagination
           currentPageIndex={pageIndex}
           totalCount={searchedNotice.length}
-          pageSize={10}
+          pageSize={pageSize}
           groupSize={10}
           onPageIndexChange={handlePageChange}
         />
