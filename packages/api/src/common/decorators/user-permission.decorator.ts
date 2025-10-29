f8c8132ecc073880ee16f0c3373ce077a4d2b7bc
litmify <litmify@gmail.com>
2025-10-29 13:24:19 +0900

    fix: make UserPermissionType to use enum

--- packages/api/src/common/decorators/user-permission.decorator.ts
+++ packages/api/src/common/decorators/user-permission.decorator.ts
@@ -1,16 +1,20 @@
 import { SetMetadata } from "@nestjs/common";
 
 export const PERMISSION_KEY = "permission";
-export type UserPermissionType =
-  | "StudentCouncilPresident"
-  | "OrganizationPresident"
-  | "OrganizationManager"
-  | "Any";
+
+export enum UserPermissionType {
+  StudentCouncilPresident = "StudentCouncilPresident",
+  OrganizationPresident = "OrganizationPresident",
+  OrganizationManager = "OrganizationManager",
+  Any = "Any",
+}
 
 export const Permission = (perm: UserPermissionType) =>
   SetMetadata(PERMISSION_KEY, perm);
 export const StudentCouncilPresident = () =>
-  Permission("StudentCouncilPresident");
-export const OrganizationPresident = () => Permission("OrganizationPresident");
-export const OrganizationManager = () => Permission("OrganizationManager");
-export const AnyUser = () => Permission("Any");
+  Permission(UserPermissionType.StudentCouncilPresident);
+export const OrganizationPresident = () =>
+  Permission(UserPermissionType.OrganizationPresident);
+export const OrganizationManager = () =>
+  Permission(UserPermissionType.OrganizationManager);
+export const AnyUser = () => Permission(UserPermissionType.Any);
