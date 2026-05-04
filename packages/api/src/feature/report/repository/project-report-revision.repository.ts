8dff1714b49d41ed6d51a13cb95e85b6b0814a5b
bean-choi <bean@sparcs.org>
2026-05-04 17:10:46 +0900

    fix: report repository

--- packages/api/src/feature/report/repository/project-report-revision.repository.ts
+++ packages/api/src/feature/report/repository/project-report-revision.repository.ts
@@ -19,13 +19,11 @@ import {
   MProjectReportRevision,
 } from "@sparcs-students/api/feature/report/model/project-report-revision.model";
 import { EmptyObject } from "@sparcs-students/api/common/base/entity.model";
-import { DocumentItemStatusEnum } from "@sparcs-students/interface/common/type/revision-base.type";
 
 export type ProjectReportRevisionQuery = {
   organizationId: number;
   semesterId: number;
   projectReportId: number;
-  documentStatusEnum: DocumentItemStatusEnum;
   submittedAt: Date;
   cogAgendaId: number;
   gsrcAgendaId: number;
@@ -85,39 +83,49 @@ export class ProjectReportRevisionRepository extends BaseMultiTableRepository<
   ): MProjectReportRevision {
     return new MProjectReportRevision({
       id: result.main.id,
+
       projectReport: { id: result.main.projectReportId },
+
       name: result.main.name,
+
       method: result.main.method,
+
       prepareDuration: {
         startTerm: result.main.prepareStartTerm,
         endTerm: result.main.prepareEndTerm,
       },
+
       duration: {
         startTerm: result.main.startTerm,
         endTerm: result.main.endTerm,
       },
+
       timelines: result.oneToMany.projectReportTimeline.map(timeline => ({
+        detail: timeline.detail,
         duration: {
           startTerm: timeline.startTerm,
           endTerm: timeline.endTerm,
         },
-        detail: timeline.detail,
         note: timeline.note,
       })),
+
       team: { id: result.main.teamId },
+
       manager: { id: result.main.managerId },
-      participation: result.main.participation,
-      // purpose: result.main.purpose,
-      // target: result.main.target,
-      result: result.main.result,
-      unmet: result.main.unmet,
+
+      purpose: result.main.purpose,
+
+      target: result.main.target,
+
       detail: result.main.detail,
+
       note: result.main.note,
-      // documentStatusEnum: result.main.documentStatusEnum,
+
       submittedAt: result.main.submittedAt,
+
       cogAgenda: { id: result.main.cogAgendaId },
+
       gsrcAgenda: { id: result.main.gsrcAgendaId },
-      // isRemoved: result.main.isRemoved,
     });
   }
 
@@ -127,27 +135,13 @@ export class ProjectReportRevisionRepository extends BaseMultiTableRepository<
     return {
       main: {
         id: model.id,
-        projectReportId: model.projectReport.id,
         name: model.name,
-        method: model.method,
-        prepareStartTerm: model.prepareDuration.startTerm,
-        prepareEndTerm: model.prepareDuration.endTerm,
-        startTerm: model.duration.startTerm,
-        endTerm: model.duration.endTerm,
         teamId: model.team.id,
         managerId: model.manager.id,
-        participation: model.participation,
-        // purpose: model.purpose,
-        // target: model.target,
-        result: model.result,
-        unmet: model.unmet,
+        purpose: model.purpose,
+        target: model.target,
         detail: model.detail,
         note: model.note,
-        // documentStatusEnum: model.documentStatusEnum,
-        submittedAt: model.submittedAt,
-        cogAgendaId: model.cogAgenda?.id,
-        gsrcAgendaId: model.gsrcAgenda?.id,
-        // isRemoved: model.isRemoved,
       },
       oneToOne: {},
       oneToMany: {
@@ -175,15 +169,10 @@ export class ProjectReportRevisionRepository extends BaseMultiTableRepository<
         endTerm: model.duration.endTerm,
         teamId: model.team.id,
         managerId: model.manager.id,
-        participation: model.participation,
-        // purpose: model.purpose,
-        // target: model.target,
-        result: model.result,
-        unmet: model.unmet,
+        purpose: model.purpose,
+        target: model.target,
         detail: model.detail,
         note: model.note,
-        // documentStatusEnum: model.documentStatusEnum,
-        // isRemoved: model.isRemoved,
       },
       oneToOne: {},
       oneToMany: {
@@ -204,20 +193,13 @@ export class ProjectReportRevisionRepository extends BaseMultiTableRepository<
       ProjectReportRevisionFieldMapKeys,
       TableWithID | null
     > = {
+      id: ProjectReportRevision,
+      organizationId: ProjectReportRevision,
+      semesterId: ProjectReportRevision,
       projectReportId: ProjectReportRevision,
-      name: ProjectReportRevision,
-      method: ProjectReportRevision,
-      prepareStartTerm: ProjectReportRevision,
-      prepareEndTerm: ProjectReportRevision,
-      startTerm: ProjectReportRevision,
-      endTerm: ProjectReportRevision,
-      teamId: ProjectReportRevision,
-      managerId: ProjectReportRevision,
-      participation: ProjectReportRevision,
-      result: ProjectReportRevision,
-      unmet: ProjectReportRevision,
-      detail: ProjectReportRevision,
-      note: ProjectReportRevision,
+      submittedAt: ProjectReportRevision,
+      cogAgendaId: ProjectReportRevision,
+      gsrcAgendaId: ProjectReportRevision,
     };
 
     if (!(field in fieldMappings)) {
